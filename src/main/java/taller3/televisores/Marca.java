6e0b3e6a8075d7fd83caf2e42ebe5a34127f17d6
jppinedal <54005283+jppinedal@users.noreply.github.com>
2022-03-28 23:45:28 -0500

    T3, P4, Jv

--- src/main/java/taller3/televisores/Marca.java
+++ src/main/java/taller3/televisores/Marca.java
@@ -3,18 +3,16 @@ package taller3.televisores;
 public class Marca {
 	private String nombre;
 	
-	public Marca(String nombre) {
-		this.nombre = nombre;
+	public Marca(String marca) {
+		this.nombre = marca;
 		
 	}
-	public String getMarca() {
-		return nombre;
-	}
-	public void setMarca(String nombre){
-		this.nombre = nombre;
-	}
+	
 	public String getNombre() {
 		return nombre;
 	}
-
+	
+	public void setNombre(String nombre){
+		this.nombre = nombre;
+	}
 }
